2b1147af44c33181aad0001edb623c673658d48a
Ian Bartlett <haku144@gmail.com>
2017-03-07 10:52:56 +1100

    Switched to Euclidian distance for decision, pole detection working

--- lab_1/ExtractOOIs.m
+++ lab_1/ExtractOOIs.m
@@ -2,12 +2,16 @@ function [ooi_list] = ExtractOOIs(range, intensity)
 % A function to sort objects in LIDAR range data into clusters
 % Ian Bartlett
 
-threshold = 0.5;
+threshold = 0.2;
 
 angles = [0:360]'*0.5*pi/180;
 
+X_pos = cos(angles).*range;
+Y_pos = sin(angles).*range;
+
+
 %Find the gaps
-object_edges = [1 find(diff(range) > threshold)'];
+object_edges = [0 (find(sqrt(diff(X_pos).^2+diff(Y_pos).^2) > threshold))'];
 
 %If the difference between the points is greater than threshold,
 %create a new object, assign to it
@@ -21,20 +25,23 @@ ooi_list = struct('N',num_obs, ...
 
 for i = 1:ooi_list.N
 
-    object_ranges = range(object_edges(i):object_edges(i+1));
-    object_angles = angles(object_edges(i):object_edges(i+1));
+  %  object_ranges = range(object_edges(i)+1:object_edges(i+1));
+  %  object_angles = angles(object_edges(i)+1:object_edges(i+1));
 
-    object_X = cos(object_angles).*object_ranges;   
-    object_Y = sin(object_angles).*object_ranges;   
+   % object_X = cos(object_angles).*object_ranges;   
+   % object_Y = sin(object_angles).*object_ranges;   
+
+    object_X = X_pos(object_edges(i)+1:object_edges(i+1));
+    object_Y = Y_pos(object_edges(i)+1:object_edges(i+1));
 
     %Use the mean of the X,Y points as the centre
-    ooi_list.Centers(:,i) = [mean(object_X),mean(object_Y)];
+    ooi_list.Centers(:,i) = [median(object_X),median(object_Y)];
 
     %Use the Euclidian distance from first to last point as the size
     ooi_list.Diameters(i) = norm([object_X(end)-object_X(1)],object_Y(end)-object_Y(1));
 
     %Check if any pixels are reflective
-    if max(intensity(object_edges(i):object_edges(i+1)) > 0)
+    if max(intensity(object_edges(i)+1:object_edges(i+1)) > 0)
 	ooi_list.Color(i) = 1;
     else
 	ooi_list.Color(i) = 0;
